bf0fd5258e7a70a5c2b85f61860a15e896b616de
Villardi2005 <gabrielvillardi@gmail.com>
2025-07-10 00:29:10 +0000

    kggk

---
+++ evasao/app/urls.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.urls import path, include
+from app import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    # path('cadastro/', views.cadastro, name='cadastro'),
+    # path('cadastroaluno', views.cadastro, name='cadastroaluno'),
+    #path('cadastroprofessor', views.cadastroprofessor, name='cadastroprofessor'),
+    #path('cadastroresponsavel', views.cadastroresponsavel, name='cadastroresponsavel'),
+    path('login', views.login, name='login'),
+    # path('redireciona/', views.redireciona_usuario, name='redireciona_usuario'),
+    # path('logout/', auth_views.LogoutView.as_view(next_page='index'), name='logout'),
+    # path('painel-responsavel/', views.painel_responsavel, name='painel_responsavel'),
+    # path('painel-professor/', views.painel_professor, name='painel_professor'),
+    # path('minhas-metas/', views.minhas_metas, name='minhas_metas'),
+    # path('admin/', admin.site.urls),
+    # #path("__reload__/", include("django_browser_reload.urls")),
+]
+
